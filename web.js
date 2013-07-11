cca3b36adba6758dffae5af18d27c3a7ab78689d
Hoe Phuan <avtinc2000@gmail.com>
2013-07-11 12:22:20 -0700

    Read index.html and display content.

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
 var express = require('express');
 
+var fs = require('fs');
+var contentStr = fs.readFileSync('index.html', 'utf8');
+//console.log( contentStr );
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(contentStr);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
